e056ac1987f74024c589afd7657d0775478706e3
wang1ang <yangwang@live.cn>
2025-10-10 00:35:58 +0900

    exit program

--- InfraView/InfraViewApp.swift
+++ InfraView/InfraViewApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct InfraViewApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     init() {
         bindDeleteToCommandBackspace()
     }
@@ -17,5 +18,13 @@ struct InfraViewApp: App {
             ContentView()
         }
         //.windowStyle(.titleBar)
+    }
+}
+
+
+// Used only for exit window by clicking the X
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true  // ✅ 当最后一个窗口被关闭时退出程序
     }
 }
